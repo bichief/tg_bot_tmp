951819380b2c0658fc51abfd27184aed8999ae3e
bichief <sashamarx67@gmail.com>
2024-01-12 13:58:29 +0300

    добавил комментарий к обработчику команды /start

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -6,4 +6,9 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def start_cmd(message: types.Message):
+    """
+    Реакция на команду /start.
+    Бот отправляет "Привет!"
+    """
+
     await message.answer('Привет!')
