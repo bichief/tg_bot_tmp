1c74ebcfb7fc8f33f5644524b4b6563b8de6013f
bichief <sashamarx67@gmail.com>
2024-01-12 13:48:04 +0300

    добавил комментарии к использованию запросов

--- bot/utils/db_api/db_commands.py
+++ bot/utils/db_api/db_commands.py
@@ -6,6 +6,16 @@ from admin_panel.telebot.models import Users
 
 @sync_to_async
 def create_user(telegram_id, name, username):
+    """
+    Функция, позволяющая создать новую запись пользователя
+    в соответствующей таблице
+
+    telegram_id = id пользователя в телеграме
+    name = имя пользователя
+    username = username пользователя
+
+    """
+
     try:
         Users.objects.get_or_create(
             telegram_id=telegram_id,
@@ -18,6 +28,21 @@ def create_user(telegram_id, name, username):
 
 @sync_to_async()
 def get_user(telegram_id):
+    """
+    Функция, позволяющая получить информацию по пользователю из БД
+    При вызове используем как объект.
+
+    Пример:
+
+    user = await get_user(message.from_user.id)
+
+    print(user.id)
+    print(user.telegram_id)
+    print(user.name)
+    print(user.username)
+
+    """
+
     try:
         return Users.objects.filter(telegram_id=telegram_id).first()
     except InterfaceError:
