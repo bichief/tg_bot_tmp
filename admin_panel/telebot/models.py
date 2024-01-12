d5be27a683e8bf2537b157c008da89499233d079
bichief <sashamarx67@gmail.com>
2024-01-12 13:10:41 +0300

    добавил абстрактную модель для добавления колонок "Дата создания" и "Дата измнения"

--- admin_panel/telebot/models.py
+++ admin_panel/telebot/models.py
@@ -4,3 +4,15 @@ from django.db import models
 User = get_user_model()
 
 
+class CreatedModel(models.Model):
+    """Абстрактная модель. Добавляет дату создания."""
+    created = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
